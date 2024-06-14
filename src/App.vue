fd228232af67cfcedcb4f4440bc92ad1ce4d706e
giordanofabrizii <giordanofabrizi@gmail.com>
2024-06-14 12:25:40 +0200

    added Header

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <script>
-  import  from '';
+  import HeaderApp from './components/HeaderApp.vue';
+  // bc32337b339c1e8a50ca13e7353af33e
 
   export default{
     components:{
-      
+      HeaderApp,
     }
   }
 </script>
 
 <template>
-  </>
+  <HeaderApp/>
 </template>
 
 <style scoped>
--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -9,7 +9,7 @@ export default{
 </script>
 
 <template>
-
+    <h1>Boolfix</h1>
 </template>
 
 <style scoped>
