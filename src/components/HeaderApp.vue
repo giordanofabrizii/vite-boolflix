1ed75be07db2fc3c410b0c7a52f35e9bd96a99db
giordanofabrizii <giordanofabrizi@gmail.com>
2024-06-14 12:35:07 +0200

    Finished header (added some style and the input)

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -9,9 +9,36 @@ export default{
 </script>
 
 <template>
-    <h1>Boolfix</h1>
+    <header>
+        <div class="limited">
+            <h1>Boolfix</h1>
+            <input type="text" placeholder="Comincia a cercare">
+        </div>
+    </header>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
+header {
+    background-color: black;
 
+    div{
+        @include flexRow;
+        justify-content: space-between;
+        padding: 1rem 0;
+
+        h1 {
+            color: $primary;
+            text-transform: uppercase;
+        }
+
+        input{
+            padding: .5rem;
+            border: none;
+            border-radius: 12px;
+        }
+    }
+
+}
 </style>
